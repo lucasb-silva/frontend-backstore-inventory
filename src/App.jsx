319188210ece5f63d464528f2a7708d709125ed7
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-10-15 17:38:30 -0300

    Criação do HTML e CSS para o Card Item do inventário

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <img src="https://media.istockphoto.com/id/960058282/pt/foto/gaming-mouse-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=xMKKwTxf5dkLexkEn2CeiqCfHDYlIyn9uO0aSofgCMQ=" alt="Mouse Gamer" width="100%" />
+        <h2>Mouse Gamer</h2>
+        <p class="quantidade">12 itens</p>
+        <p>Mouse Gamer de ultima geração...</p>
+        <p><button>Editar item</button></p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
