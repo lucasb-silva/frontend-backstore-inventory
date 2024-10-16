e25d70f4c601fda185f7597900bb38d62df47b3c
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-10-16 16:36:37 -0300

    CSS extraido para o `Product.css`

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,13 +1,15 @@
+import './Product.css'
+
 export default function Product() {
   return (
-    <div className="card">
+    <div className="product">
       <img
         src="https://media.istockphoto.com/id/960058282/pt/foto/gaming-mouse-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=xMKKwTxf5dkLexkEn2CeiqCfHDYlIyn9uO0aSofgCMQ="
         alt="Mouse Gamer"
         width="100%"
       />
       <h2>Mouse Gamer</h2>
-      <p class="quantidade">12 itens</p>
+      <p class="quantity">12 itens</p>
       <p>Mouse Gamer de ultima geração...</p>
       <p>
         <button>Editar item</button>
