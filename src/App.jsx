0c61bd494b770b59bce86b83bda6aeaaec6a8bb2
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-10-16 16:59:39 -0300

    Configuração das `props` no componente `<Product />`

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import './App.css'
-import Product from './components/Product/Product'
+import "./App.css";
+import Product from "./components/Product/Product";
 
 function App() {
   return (
     <>
-      <Product />
-      <Product />
-      
+      <Product
+        name="Mouse Gamer"
+        image="https://media.istockphoto.com/id/960058282/pt/foto/gaming-mouse-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=xMKKwTxf5dkLexkEn2CeiqCfHDYlIyn9uO0aSofgCMQ="
+        quantity="10"
+        shortDescription="Mouse Gamer de ultima geração"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,16 +1,16 @@
 import './Product.css'
 
-export default function Product() {
+export default function Product(props) {
   return (
     <div className="product">
       <img
-        src="https://media.istockphoto.com/id/960058282/pt/foto/gaming-mouse-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=xMKKwTxf5dkLexkEn2CeiqCfHDYlIyn9uO0aSofgCMQ="
-        alt="Mouse Gamer"
+        src={props.image}
+        alt={props.name}
         width="100%"
       />
-      <h2>Mouse Gamer</h2>
-      <p class="quantity">12 itens</p>
-      <p>Mouse Gamer de ultima geração...</p>
+      <h2>{props.name}</h2>
+      <p class="quantity">{props.quantity} itens</p>
+      <p>{props.shortDescription}...</p>
       <p>
         <button>Editar item</button>
       </p>
