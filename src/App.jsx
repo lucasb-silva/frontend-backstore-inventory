71192dce895e4cb9310517b0d54f9d623d7ae8d0
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-10-16 17:53:43 -0300

    Elementos do inventário com estilização adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ import Product from "./components/Product/Product";
 function App() {
   return (
     <>
-      <Product
-        name="Mouse Gamer"
-        image="https://media.istockphoto.com/id/960058282/pt/foto/gaming-mouse-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=xMKKwTxf5dkLexkEn2CeiqCfHDYlIyn9uO0aSofgCMQ="
-        quantity="10"
-        shortDescription="Mouse Gamer de ultima geração"
-      />
+      <div className="inventory">
+        <Product name="Mouse Gamer" image="https://media.istockphoto.com/id/960058282/pt/foto/gaming-mouse-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=xMKKwTxf5dkLexkEn2CeiqCfHDYlIyn9uO0aSofgCMQ=" quantity="10" shortDescription="Mouse Gamer de ultima geração" />
+        <Product name="Mouse Gamer" image="https://media.istockphoto.com/id/960058282/pt/foto/gaming-mouse-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=xMKKwTxf5dkLexkEn2CeiqCfHDYlIyn9uO0aSofgCMQ=" quantity="10" shortDescription="Mouse Gamer de ultima geração" />
+        <Product name="Mouse Gamer" image="https://media.istockphoto.com/id/960058282/pt/foto/gaming-mouse-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=xMKKwTxf5dkLexkEn2CeiqCfHDYlIyn9uO0aSofgCMQ=" quantity="10" shortDescription="Mouse Gamer de ultima geração" />
+      </div>
     </>
   );
 }
