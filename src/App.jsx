6522552b26338464bd65e61f8e3d82d0d3a05a51
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-10-16 16:31:22 -0300

    Criação do componente `<Product />`

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
+import Product from './components/Product/Product'
 
 function App() {
   return (
     <>
-      <div className="card">
-      <img src="https://media.istockphoto.com/id/960058282/pt/foto/gaming-mouse-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=xMKKwTxf5dkLexkEn2CeiqCfHDYlIyn9uO0aSofgCMQ=" alt="Mouse Gamer" width="100%" />
-        <h2>Mouse Gamer</h2>
-        <p class="quantidade">12 itens</p>
-        <p>Mouse Gamer de ultima geração...</p>
-        <p><button>Editar item</button></p>
-      </div>
+      <Product />
+      <Product />
+      
     </>
   )
 }
