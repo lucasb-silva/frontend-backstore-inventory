ef249d95460cd174343dbd7451db563fee2efb90
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-10-17 02:27:52 -0300

    Lista do inventário carregada via API com Fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
 import "./App.css";
 import Card from "./components/Product/Card";
 
-function App() {
-  const product = {
-    name: "Mouse Gamer",
-    image:
-      "https://media.istockphoto.com/id/960058282/pt/foto/gaming-mouse-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=xMKKwTxf5dkLexkEn2CeiqCfHDYlIyn9uO0aSofgCMQ=",
-    quantity: "12",
-    shortDescription: "Mouse Gamer de ultima geração",
-  };
-  const product1 = {
-    name: "Teclado Gamer",
-    image:
-      "https://media.istockphoto.com/id/960058282/pt/foto/gaming-mouse-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=xMKKwTxf5dkLexkEn2CeiqCfHDYlIyn9uO0aSofgCMQ=",
-    quantity: "12",
-    shortDescription: "Mouse Gamer de ultima geração",
-  };
-  const product2 = {
-    name: "Headphone",
-    quantity: "12",
-    shortDescription: "Mouse Gamer de ultima geração",
-  };
+async function App() {
+  
+  const inventory = []
+  
+  async function fetchData() {
+    const apiUrl = 'http://localhost:5000/inventario/'
 
-  const inventory = [product, product1, product2];
+    const response = await fetch(apiUrl)
+
+    const data = await response.json()
+
+    console.log(45, data)
+  }
+
+  fetchData()
 
   return (
     <>
