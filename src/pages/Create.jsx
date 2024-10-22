1843c7f380da2fa83cedd185d250c800b149c8ed
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-10-22 17:59:42 -0300

    Criação do `<form>`

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,3 +1,33 @@
 export default function Create() {
-  return <div>Create</div>
+  return (
+    <div>
+      <h3>Novo produto:</h3>
+      <form>
+        <div>
+          <label htmlFor="nome">Nome *:</label><br />
+          <input type="text" id="nome" name="nome" placeholder="Insira o nome do produto" />
+        </div>
+
+        <div>
+          <label htmlFor="nome">Imagem (URL):</label><br />
+          <input type="text" id="imagem" name="imagem" placeholder="Insira a URL da imagem" />
+        </div>
+
+        <div>
+          <label htmlFor="nome">Quantidade *:</label><br />
+          <input type="text" id="quantidade" name="quantidade" placeholder="Insira a quantidade" />
+        </div>
+
+        <div>
+          <label htmlFor="nome">Descrição *:</label><br />
+          <input type="text" id="descrição" name="descrição" placeholder="Insira a quantidade" />
+        </div>
+
+        <div>
+          <button type="submit">Criar</button>
+          <button type="reset">Reiniciar</button>
+        </div>
+      </form>
+    </div>
+  )
 }
